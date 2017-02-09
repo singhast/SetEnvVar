8ca319d70c52dbc03c33215a1a587b399e02a29d
singhast <singh.abhi@outlook.in>
2017-02-09 22:01:10 +0530

    Added basic Program structure

---
+++ SetEnvVar/SetEnvVar/ExecOptions.cs
@@ -0,0 +1,74 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace SetEnvVar
+{
+    class ExecOptions
+    {
+        [Option(longName: "project", Required = true)]
+        public string Project
+        {
+            get;
+            set;
+        }
+
+        [Option(longName: "set", Required = true)]
+        public string Variable
+        {
+            get;
+            set;
+        }
+
+        [Option(longName: "type", Required = true)]
+        public string VariableType
+        {
+            get;
+            set;
+        }
+
+
+        [Option(longName: "KeepEncrypted", Required = false)]
+        public bool KeepEncrypted
+        {
+            get;
+            set;
+        }
+
+
+        [Option(longName: "regex", Required = false)]
+        public string ValueRegex
+        {
+            get;
+            set;
+        }
+
+        [Option('h', "help", Required = false, HelpText = "Print help")]
+        public bool IsHelpOperation
+        {
+            get;
+            set;
+        }
+
+
+
+        [HelpOption]
+        internal string getusage()
+        {
+            var usage = new StringBuilder();
+
+            usage.AppendLine(string.Format("SetEnvVar {0}", Assembly.GetExecutingAssembly().GetName().Version));
+            usage.AppendLine(" Usage Help");
+            usage.AppendLine("   odbcman --help ");
+            usage.AppendLine("   odbcman -h ");
+
+            return usage.ToString();
+
+        }
+
+
+    }
+}
---
+++ SetEnvVar/SetEnvVar/Program.cs
@@ -0,0 +1,75 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SetEnvVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParserSettings parserSettings = new ParserSettings();
+            Parser parser = new Parser(parserSettings);
+
+            ExecOptions options = new ExecOptions();
+
+            parser.ParseArguments(args, options);
+
+            string[] problems = Validate(options);
+            if (problems.Length == 0)
+                HandleOptions(options);
+            else
+            {
+                foreach (string problem in problems)
+                    Console.WriteLine(problems);
+                Console.WriteLine(options.getusage());
+            }
+        }
+
+        private static string[] Validate(ExecOptions options)
+        {
+            List<string> issues = new List<string>();
+            if (!string.IsNullOrWhiteSpace(options.Project))
+            {
+                if (!string.IsNullOrWhiteSpace(options.Variable))
+                {
+                    if (!string.IsNullOrWhiteSpace(options.VariableType))
+                    {
+                        switch (options.VariableType.ToLower())
+                        {
+                            case "file": 
+                                break;
+                            case "value": 
+                                break;
+                            case "folder": ;
+                                break;
+                            default:
+                                issues.Add(string.Format("Unknown Type:{0", options.VariableType));
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        issues.Add("Variable Type is required");
+                    }
+
+                }
+                else
+                    issues.Add("Variable is required");
+
+            }
+            else
+                issues.Add("Project is required");
+
+            return issues.ToArray();
+
+        }
+
+        private static void HandleOptions(ExecOptions options)
+        {
+
+        }
+    }
+}
